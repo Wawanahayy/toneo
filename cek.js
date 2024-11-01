2aa3fcbefc717b5324aac83d4a622bd74c9a4f9f
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-11-01 09:25:08 +0700

    Update cek.js

--- cek.js
+++ cek.js
@@ -3,7 +3,6 @@ const { promisify } = require('util');
 const fs = require('fs');
 const readline = require('readline');
 const axios = require('axios');
-const { exec } = require('child_process'); // Import exec untuk menjalankan skrip Bash
 
 let socket = null;
 let pingInterval;
@@ -22,252 +21,251 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// Menjalankan skrip Bash
-const runBashScript = () => {
-  exec('bash display.sh', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing script: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Script stderr: ${stderr}`);
-      return;
-    }
-    console.log(stdout); // Tampilkan output dari skrip Bash
-  });
-};
+// Fungsi untuk mencetak teks berwarna
+function printColored(colorCode, text) {
+    console.log(`\x1b[${colorCode}m${text}\x1b[0m`);
+}
 
-// Menampilkan header menggunakan curl
-const displayHeader = async () => {
-  const header = await axios.get('https://raw.githubusercontent.com/Wawanahayy/JawaPride-all.sh/refs/heads/main/display.sh');
-  console.log(header.data);
-};
+// Fungsi untuk menampilkan teks berwarna
+function displayColoredText() {
+    printColored("40;96", "============================================================");
+    printColored("42;37", "=======================  J.W.P.A  ==========================");
+    printColored("45;97", "================= @AirdropJP_JawaPride =====================");
+    printColored("43;30", "=============== https://x.com/JAWAPRIDE_ID =================");
+    printColored("41;97", "============= https://linktr.ee/Jawa_Pride_ID ==============");
+    printColored("44;30", "============================================================");
+}
+
+// Menampilkan header
+displayColoredText();
+setTimeout(() => {
+    console.clear();
+}, 5000);
 
 async function getLocalStorage() {
-  try {
-    const data = await readFileAsync('localStorage.json', 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    return {};
-  }
+    try {
+        const data = await readFileAsync('localStorage.json', 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        return {};
+    }
 }
 
 async function setLocalStorage(data) {
-  const currentData = await getLocalStorage();
-  const newData = { ...currentData, ...data };
-  await writeFileAsync('localStorage.json', JSON.stringify(newData));
+    const currentData = await getLocalStorage();
+    const newData = { ...currentData, ...data };
+    await writeFileAsync('localStorage.json', JSON.stringify(newData));
 }
 
 async function connectWebSocket(userId) {
-  if (socket) return;
-  const version = "v0.2";
-  const url = "wss://secure.ws.teneo.pro";
-  const wsUrl = `${url}/websocket?userId=${encodeURIComponent(userId)}&version=${encodeURIComponent(version)}`;
-  socket = new WebSocket(wsUrl);
+    if (socket) return;
+    const version = "v0.2";
+    const url = "wss://secure.ws.teneo.pro";
+    const wsUrl = `${url}/websocket?userId=${encodeURIComponent(userId)}&version=${encodeURIComponent(version)}`;
+    socket = new WebSocket(wsUrl);
 
-  socket.onopen = async () => {
-    const connectionTime = new Date();
-    const formattedConnectionTime = formatDate(connectionTime);
-    await setLocalStorage({ lastUpdated: connectionTime.toISOString() });
-    console.log("WebSocket connected at", formattedConnectionTime);
-    startPinging();
-    startCountdownAndPoints();
-    startLogUpdates();
-  };
+    socket.onopen = async () => {
+        const connectionTime = new Date();
+        const formattedConnectionTime = formatDate(connectionTime);
+        await setLocalStorage({ lastUpdated: connectionTime.toISOString() });
+        console.log("WebSocket connected at", formattedConnectionTime);
+        startPinging();
+        startCountdownAndPoints();
+        startLogUpdates();
+    };
 
-  socket.onmessage = async (event) => {
-    const data = JSON.parse(event.data);
-    const messageTime = new Date(data.date);
-    const formattedMessageTime = formatDate(messageTime);
-    
-    data.DATE = formattedMessageTime;
-    console.log(`Received message from WebSocket:`, {
-      ...data,
-      currentTime: formatDate(new Date())
-    });
+    socket.onmessage = async (event) => {
+        const data = JSON.parse(event.data);
+        const messageTime = new Date(data.date);
+        const formattedMessageTime = formatDate(messageTime);
+        
+        data.DATE = formattedMessageTime;
+        console.log(`Received message from WebSocket:`, {
+            ...data,
+            currentTime: formatDate(new Date())
+        });
 
-    if (data.pointsTotal !== undefined && data.pointsToday !== undefined) {
-      const lastUpdated = new Date().toISOString();
-      await setLocalStorage({
-        lastUpdated: lastUpdated,
-        pointsTotal: data.pointsTotal,
-        pointsToday: data.pointsToday,
-      });
-      pointsTotal = data.pointsTotal;
-      pointsToday = data.pointsToday;
-    }
-  };
+        if (data.pointsTotal !== undefined && data.pointsToday !== undefined) {
+            const lastUpdated = new Date().toISOString();
+            await setLocalStorage({
+                lastUpdated: lastUpdated,
+                pointsTotal: data.pointsTotal,
+                pointsToday: data.pointsToday,
+            });
+            pointsTotal = data.pointsTotal;
+            pointsToday = data.pointsToday;
+        }
+    };
 
-  socket.onclose = () => {
-    socket = null;
-    console.log("WebSocket disconnected");
-    stopPinging();
-    stopLogUpdates();
-  };
+    socket.onclose = () => {
+        socket = null;
+        console.log("WebSocket disconnected");
+        stopPinging();
+        stopLogUpdates();
+    };
 
-  socket.onerror = (error) => {
-    console.error("WebSocket error:", error);
-  };
+    socket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+    };
 }
 
 function disconnectWebSocket() {
-  if (socket) {
-    socket.close();
-    socket = null;
-    stopPinging();
-    stopLogUpdates();
-  }
+    if (socket) {
+        socket.close();
+        socket = null;
+        stopPinging();
+        stopLogUpdates();
+    }
 }
 
 function startPinging() {
-  stopPinging();
-  pingInterval = setInterval(async () => {
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      socket.send(JSON.stringify({ type: "PING" }));
-      await setLocalStorage({ lastPingDate: new Date().toISOString() });
-    }
-  }, 10000);
+    stopPinging();
+    pingInterval = setInterval(async () => {
+        if (socket && socket.readyState === WebSocket.OPEN) {
+            socket.send(JSON.stringify({ type: "PING" }));
+            await setLocalStorage({ lastPingDate: new Date().toISOString() });
+        }
+    }, 10000);
 }
 
 function stopPinging() {
-  if (pingInterval) {
-    clearInterval(pingInterval);
-    pingInterval = null;
-  }
+    if (pingInterval) {
+        clearInterval(pingInterval);
+        pingInterval = null;
+    }
 }
 
 function startLogUpdates() {
-  stopLogUpdates();
-  logInterval = setInterval(async () => {
-    const localStorageData = await getLocalStorage();
-    console.log(`Log Update: \n - Points Today: ${localStorageData.pointsToday || 0} \n - Points Total: ${localStorageData.pointsTotal || 0} \n - Potential Points: ${potentialPoints}`);
-  }, 300000);
+    stopLogUpdates();
+    logInterval = setInterval(async () => {
+        const localStorageData = await getLocalStorage();
+        console.log(`Log Update: \n - Points Today: ${localStorageData.pointsToday || 0} \n - Points Total: ${localStorageData.pointsTotal || 0} \n - Potential Points: ${potentialPoints}`);
+    }, 300000);
 }
 
 function stopLogUpdates() {
-  if (logInterval) {
-    clearInterval(logInterval);
-    logInterval = null;
-  }
+    if (logInterval) {
+        clearInterval(logInterval);
+        logInterval = null;
+    }
 }
 
 process.on('SIGINT', () => {
-  console.log('Received SIGINT. Stopping pinging and log updates...');
-  stopPinging();
-  stopLogUpdates();
-  disconnectWebSocket();
-  process.exit(0);
+    console.log('Received SIGINT. Stopping pinging and log updates...');
+    stopPinging();
+    stopLogUpdates();
+    disconnectWebSocket();
+    process.exit(0);
 });
 
 let currentColorIndex = 0;
 const colors = ['\x1b[31m', '\x1b[32m', '\x1b[33m', '\x1b[34m', '\x1b[35m', '\x1b[36m', '\x1b[37m', '\x1b[0m'];
 
 function updateBlinkingColorMessage() {
-  console.clear(); 
-  const currentTime = formatDate(new Date());
-  const websocketStatus = socket && socket.readyState === WebSocket.OPEN ? 'Connected' : 'Disconnected'; 
-  console.log(`---------------------`);
-  console.log(`${colors[currentColorIndex]}Waktu Saat Ini: ${currentTime}\x1b[0m`); 
-  console.log(`${colors[currentColorIndex]}Poin Hari Ini: ${pointsToday}\x1b[0m`); 
-  console.log(`${colors[currentColorIndex]}Total Poin: ${pointsTotal}\x1b[0m`); 
-  console.log(`${colors[currentColorIndex]}Websocket: ${websocketStatus}\x1b[0m`); 
-  console.log(`${colors[currentColorIndex]}FOLLOW TG: @AirdropJP_JawaPride\x1b[0m`); 
-  console.log(`---------------------`);
-  currentColorIndex = (currentColorIndex + 1) % colors.length;
+    console.clear(); 
+    const currentTime = formatDate(new Date());
+    const websocketStatus = socket && socket.readyState === WebSocket.OPEN ? 'Connected' : 'Disconnected'; 
+    console.log(`---------------------`);
+    console.log(`${colors[currentColorIndex]}Waktu Saat Ini: ${currentTime}\x1b[0m`); 
+    console.log(`${colors[currentColorIndex]}Poin Hari Ini: ${pointsToday}\x1b[0m`); 
+    console.log(`${colors[currentColorIndex]}Total Poin: ${pointsTotal}\x1b[0m`); 
+    console.log(`${colors[currentColorIndex]}Websocket: ${websocketStatus}\x1b[0m`); 
+    console.log(`${colors[currentColorIndex]}FOLLOW TG: @AirdropJP_JawaPride\x1b[0m`); 
+    console.log(`---------------------`);
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
 }
 
 function startCountdownAndPoints() {
-  clearInterval(countdownInterval);
-  updateCountdownAndPoints();
-  countdownInterval = setInterval(() => {
+    clearInterval(countdownInterval);
     updateCountdownAndPoints();
-    updateBlinkingColorMessage();
-  }, 1000);
+    countdownInterval = setInterval(() => {
+        updateCountdownAndPoints();
+        updateBlinkingColorMessage();
+    }, 1000);
 }
 
 async function updateCountdownAndPoints() {
-  const { lastUpdated } = await getLocalStorage();
-  if (lastUpdated) {
-    const nextHeartbeat = new Date(lastUpdated);
-    nextHeartbeat.setMinutes(nextHeartbeat.getMinutes() + 15);
-    const now = new Date();
-    const diff = nextHeartbeat.getTime() - now.getTime();
+    const { lastUpdated } = await getLocalStorage();
+    if (lastUpdated) {
+        const nextHeartbeat = new Date(lastUpdated);
+        nextHeartbeat.setMinutes(nextHeartbeat.getMinutes() + 15);
+        const now = new Date();
+        const diff = nextHeartbeat.getTime() - now.getTime();
 
-    if (diff > 0) {
-      const minutes = Math.floor(diff / 60000);
-      const seconds = Math.floor((diff % 60000) / 1000);
-      countdown = `${minutes}m ${seconds}s`;
+        if (diff > 0) {
+            const minutes = Math.floor(diff / 60000);
+            const seconds = Math.floor((diff % 60000) / 1000);
+            countdown = `${minutes}m ${seconds}s`;
 
-      const maxPoints = 25;
-      const timeElapsed = now.getTime() - new Date(lastUpdated).getTime();
-      const timeElapsedMinutes = timeElapsed / (60 * 1000);
-      let newPoints = Math.min(maxPoints, (timeElapsedMinutes / 15) * maxPoints);
-      newPoints = parseFloat(newPoints.toFixed(2));
+            const maxPoints = 25;
+            const timeElapsed = now.getTime() - new Date(lastUpdated).getTime();
+            const timeElapsedMinutes = timeElapsed / (60 * 1000);
+            let newPoints = Math.min(maxPoints, (timeElapsedMinutes / 15) * maxPoints);
+            newPoints = parseFloat(newPoints.toFixed(2));
 
-      if (Math.random() < 0.1) {
-        const bonus = Math.random() * 2;
-        newPoints = Math.min(maxPoints, newPoints + bonus);
-        newPoints = parseFloat(newPoints.toFixed(2));
-      }
+            if (Math.random() < 0.1) {
+                const bonus = Math.random() * 2;
+                newPoints = Math.min(maxPoints, newPoints + bonus);
+                newPoints = parseFloat(newPoints.toFixed(2));
+            }
 
-      potentialPoints = newPoints;
+            potentialPoints = newPoints;
+        } else {
+            countdown = "Calculating...";
+            potentialPoints = 25;
+        }
     } else {
-      countdown = "Calculating...";
-      potentialPoints = 25;
+        countdown = "Calculating...";
+        potentialPoints = 0;
     }
-  } else {
-    countdown = "Calculating...";
-    potentialPoints = 0;
-  }
 
-  await setLocalStorage({ potentialPoints, countdown });
+    await setLocalStorage({ potentialPoints, countdown });
 }
 
 async function getUserId() {
-  const loginUrl = "https://ikknngrgxuxgjhplbpey.supabase.co/auth/v1/token?grant_type=password";
-  const authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
-  const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
-  const userId = await getLocalStorage().then(data => data.userId);
+    const loginUrl = "https://ikknngrgxuxgjhplbpey.supabase.co/auth/v1/token?grant_type=password";
+    const authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
+    const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
+    const userId = await getLocalStorage().then(data => data.userId);
 
-  if (userId) {
-    connectWebSocket(userId);
-  } else {
-    rl.question('Masukkan email: ', (email) => {
-      rl.question('Masukkan password: ', (password) => {
-        axios.post(loginUrl, { email, password }, {
-          headers: {
-            'Authorization': authorization,
-            'apikey': apikey,
-            'Content-Type': 'application/json',
-          }
-        }).then(async (response) => {
-          const userId = response.data.user.id;
-          await setLocalStorage({ userId });
-          connectWebSocket(userId);
-        }).catch(error => {
-          console.error('Error saat login:', error.message);
-        }).finally(() => {
-          rl.close();
+    if (userId) {
+        connectWebSocket(userId);
+    } else {
+        rl.question('Masukkan email: ', (email) => {
+            rl.question('Masukkan password: ', (password) => {
+                axios.post(loginUrl, { email, password }, {
+                    headers: {
+                        'Authorization': authorization,
+                        'apikey': apikey,
+                        'Content-Type': 'application/json',
+                    }
+                }).then(async (response) => {
+                    const userId = response.data.user.id;
+                    await setLocalStorage({ userId });
+                    connectWebSocket(userId);
+                }).catch(error => {
+                    console.error('Error saat login:', error.message);
+                }).finally(() => {
+                    rl.close();
+                });
+            });
         });
-      });
-    });
-  }
+    }
 }
 
 // Format tanggal
 function formatDate(date) {
-  return date.toLocaleString('id-ID', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-  });
+    return date.toLocaleString('id-ID', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    });
 }
 
 // Menjalankan fungsi
-runBashScript(); // Menjalankan skrip Bash saat memulai
-displayHeader(); // Menampilkan header
+displayColoredText(); // Menampilkan header
 getUserId(); // Meminta input pengguna dan memulai koneksi WebSocket
