28bf2947e0864f84c7aa7c4b382428f36681dcda
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-11-04 08:28:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const WebSocket = require('ws');
 const { promisify } = require('util');
 const fs = require('fs');
 const axios = require('axios');
-const HttpsProxyAgent = require('https-proxy-agent'); // Pastikan mengimpor dengan benar
+const { HttpsProxyAgent } = require('https-proxy-agent'); // Ubah ini
 
 let socket = null;
-let startTime; // Untuk menyimpan waktu mulai
+let startTime;
 
 const readFileAsync = promisify(fs.readFile);
 
@@ -26,15 +26,15 @@ async function connectWebSocket(userId, proxy) {
   const wsUrl = `${url}/websocket?userId=${encodeURIComponent(userId)}&version=${encodeURIComponent(version)}`;
 
   // Mengatur proxy di WebSocket
-  const agent = proxy ? new HttpsProxyAgent(proxy) : undefined; // Pastikan menggunakan constructor dengan benar
-  
+  const agent = proxy ? new HttpsProxyAgent(proxy) : undefined;
+
   const wsOptions = {
-    agent // Menyertakan agent jika ada
+    agent
   };
 
   socket = new WebSocket(wsUrl, wsOptions);
 
-  startTime = new Date(); // Menyimpan waktu mulai saat koneksi WebSocket
+  startTime = new Date();
 
   socket.onopen = () => {
     console.log("WebSocket connected");
@@ -57,8 +57,8 @@ async function connectWebSocket(userId, proxy) {
 
 async function getUserId(account, index) {
   const loginUrl = "https://ikknngrgxuxgjhplbpey.supabase.co/auth/v1/token?grant_type=password";
-  const authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
-  const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
+  const authorization = "Bearer YOUR_AUTHORIZATION_TOKEN"; // Ganti dengan token otorisasi yang sesuai
+  const apikey = "YOUR_API_KEY"; // Ganti dengan API key yang sesuai
 
   const email = account.email;
   const password = account.password;
