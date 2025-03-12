952821aaf23b397df65de1ca723e034be04d3a1b
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2025-03-13 06:36:06 +0700

    Update run.js

--- run.js
+++ run.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const readline = require('readline');
 const keypress = require('keypress');
 const delay = require('delay');
-const { displayAllAccounts } = require('./a');
 
 
 let sockets = [];
